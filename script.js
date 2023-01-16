cfa0b227b44bf762c92e330bdf1465c16a98a171
sonny <mpcasey@gmail.com>
2023-01-16 12:12:59 -0600

    Added if statements and else if statements to help formulate logic of the rock, paper, scissors game.

--- script.js
+++ script.js
@@ -19,4 +19,43 @@ window.onload = function() {
 function selectChoice() {
   you = this.id;
   document.getElementById("your-choice").src = you + ".png";
+
+  //random for opponent
+  opponent = choices[Math.floor(Math.random() * 3)]; 
+  // gives random number between 0- .999999 * 3 = 0-2.999999. Math.floor gets rid of any decimal numbers and keeps the whole numbers. This becomes 0-2 and randomly chooses any of the var choices array at line 6.
+  document.getElementById("opponent-choice").src = opponent + ".png";
+
+  //check for winner
+  if (you == opponent) {
+    yourScore += 1;
+    opponentScore += 1;
+  }
+  else {
+    if (you == "rock") {
+        if (opponent == "scissors") {
+          yourScore += 1;
+        } 
+        else if (opponent == "paper") {
+          opponentScore += 1;
+        }
+    }
+    if (you == "paper") {
+        if (opponent == "rock") {
+          yourScore += 1;
+        }
+        else if (opponent == "scissors") {
+          opponentScore += 1;
+        }
+    }
+    if (you == "scissors") {
+        if (opponent == "paper") {
+          yourScore += 1;
+        }
+        else if (opponent == "rock") {
+          opponentScore += 1;
+        }
+    }
+  }
+  document.getElementById("your-score").innerText = yourScore;
+  document.getElementById("opponent-score").innerText = opponentScore;
 }
